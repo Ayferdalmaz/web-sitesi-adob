b6da2ea610277d1229fdb076ba907287b718a0c8
Ayferdalmaz <36923629+Ayferdalmaz@users.noreply.github.com>
2018-05-16 14:20:45 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DALMAZ İNŞAAT</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="index.css">
+
+
+</head>
+<body style="font-family:Arial,Helvetica">
+<div class="container" style="background-color:#87CEEB;width:100%;height:700px">
+<header style="background-color:#6A5ACD;width:100%;height:100px">
+	<div style="float-left">
+		<h1 style="color:#00FFF;margin-left:100px;float:left;padding:10px"><span style="color:#e8491d">DALMAZ  İNŞAAT</span></h1>
+	</div>
+	<div style="float:right;margin-right:30px">
+		<ul style="list-style:none">
+			<li style="float:left;padding:25px">Anasayfa</li>
+			<li style="float:left;padding:25px">Hakkımızda</li>
+			<li style="float:left;padding:25px">Referanslarımız</li>
+			<li style="float:left;padding:25px">Projelerimiz</li>
+			<li style="float:left;padding:25px">Galeri</li>
+			<li style="float:left;padding:25px">E-Katolog</li>
+		</ul>
+		
+		
+	</div>
+</header>	
+		<div class="header" style="font:15px Arial,Helvetica,sans-serif;margin-left:850px;float-left:30px;padding:0px;" >
+		<h1>LOREM IPSUM NEDİR?</h1>
+		<p1>Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir.
+		Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak 
+		üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı 
+		sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, 
+		aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları
+		da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem 
+		Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.</p1>
+		</div>
+		<div style="font:15px Arial,Helvetica,sans-serif;margin-left:850px;float-left:30px;padding:0px;">
+		<h2>NEREDEN GELİR?</h2>
+		<p2>Yaygın inancın tersine, Lorem Ipsum rastgele sözcüklerden oluşmaz. Kökleri M.Ö. 45 tarihinden bu 
+		yana klasik Latin edebiyatına kadar uzanan 2000 yıllık bir geçmişi vardır. Virginia'daki Hampden-Sydney 
+		College'dan Latince profesörü Richard McClintock, bir Lorem Ipsum pasajında geçen ve anlaşılması en güç 
+		sözcüklerden biri olan 'consectetur' sözcüğünün klasik edebiyattaki örneklerini incelediğinde kesin bir
+		kaynağa ulaşmıştır. Lorm Ipsum, Çiçero tarafından M.Ö. 45 tarihinde kaleme alınan "de Finibus Bonorum et
+		Malorum" (İyi ve Kötünün Uç Sınırları) eserinin 1.10.32 ve 1.10.33 sayılı bölümlerinden gelmektedir.</p2>
+		</div>
+</div>
+
+</body>
+
+</html>
